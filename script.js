75befda80a36354f548fc1bea6879bf526f5dc0f
Mohammed Pasha <mk.pasha@hotmail.com>
2017-03-14 18:08:30 -0400

    Add files via upload
    Quick fix for ALTER command generation

---
+++ script.js
@@ -0,0 +1,15 @@
+function stringParser() {
+    document.getElementById("data").innerHTML = "";
+    document.getElementById("alterCMD").innerHTML = "";
+
+    var password = document.getElementById("pwd").value;
+    var documentInput = document.getElementById("input").value;
+
+    var splitText = documentInput.split("\n");
+
+    document.getElementById("data").innerHTML = password + "<br><br>" + splitText;
+
+    for (var i = 0; i < splitText.length; i++) {
+        document.getElementById("alterCMD").innerHTML = document.getElementById("alterCMD").innerHTML + "ALTER USER \"" + splitText[i] + "\" PROFILE \"ENDUSER2\" IDENTIFIED BY \"" + password + "\" account unlock;<br>" ;
+    }
+}
